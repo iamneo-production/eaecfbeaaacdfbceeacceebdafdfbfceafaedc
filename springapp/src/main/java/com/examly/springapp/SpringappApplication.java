29c7b79822c0d95b20dbdecb977f9c06ae7d944e
Iamneo <techadmin@iamneo.ai>
2023-08-06 20:08:01 +0000

    Backup Commit

--- springapp/src/main/java/com/examly/springapp/SpringappApplication.java
+++ springapp/src/main/java/com/examly/springapp/SpringappApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringappApplication {
+public class TaskManagementAppApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringappApplication.class, args);
+		SpringApplication.run(TaskManagementAppApplication.class, args);
 	}
 
 }
